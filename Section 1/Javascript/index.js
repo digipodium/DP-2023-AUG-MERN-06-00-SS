a81e08149109d0f19f4f19c9fd917a9d20a61f31
yourusername <triplem656@gmail.com>
2023-09-16 20:47:35 +0530

    javascript

---
+++ Section 1/Javascript/index.js
@@ -0,0 +1,20 @@
+console.log('Hello JavaScript');
+
+num = 465;
+
+console.log( num );
+
+myname = "Mubassir";
+
+console.log(myname);
+
+age = 63;
+
+console.log(age);
+
+isHungry = true;
+
+console.log(isHungry);
+
+console.log( typeof isHungry );
+
