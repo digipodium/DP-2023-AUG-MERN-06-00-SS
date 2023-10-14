e512dd468bd837fd712a92d86e68ef2a502de1b3
m-triple-m <triplem656@gmail.com>
2023-10-14 18:45:25 +0530

    signup form

--- Section 2/frontend/src/components/Signup.jsx
+++ Section 2/frontend/src/components/Signup.jsx
@@ -2,8 +2,24 @@ import React from 'react'
 
 const Signup = () => {
   return (
-    <div>
-      <h1>Register Page</h1>
+    <div className='vh-100 bg-body-secondary'>
+      <div className='col-md-4 mx-auto py-5'>
+          <div className="card">
+            <div className="card-body">
+              <h2 className='text-center my-5'>SIGNUP FORM</h2>
+              <form>
+                <label>Full Name</label>
+                <input type="text" className="form-control mb-3" />
+                <label>Email</label>
+                <input type="text" className="form-control mb-3" />
+                <label>Password</label>
+                <input type="password" className="form-control mb-3" />
+
+                <button className='btn btn-primary mt-3'>Submit</button>
+              </form>
+            </div>
+          </div>
+      </div>
     </div>
   )
 }
