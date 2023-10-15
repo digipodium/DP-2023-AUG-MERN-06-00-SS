179be0cddeabbe125f9b052fef97392d089b4647
m-triple-m <triplem656@gmail.com>
2023-10-15 18:14:11 +0530

    form handling

--- Section 2/frontend/src/components/Signup.jsx
+++ Section 2/frontend/src/components/Signup.jsx
@@ -1,21 +1,49 @@
-import React from 'react'
+import { useFormik } from 'formik';
+import React from 'react';
+import * as Yup from 'yup';
+
+const SignupSchema = Yup.object().shape({
+  email: Yup.string().email('Invalid email').required('Required'),
+  name : Yup.string().required('Name is required').min(3, 'Too Short'),
+  password: Yup.string().required('Password is required').min(6, 'Too Short')
+  .matches('[a-zA-Z]', 'Password is invalid')
+});
 
 const Signup = () => {
+
+
+
+  // initialize formik
+  const signupForm = useFormik({
+    initialValues:{
+      name : '',
+      email: '',
+      password: ''
+    },
+
+    onSubmit: (values) => { console.log(values) },
+
+    validationSchema: SignupSchema
+  
+  })
+
   return (
     <div className='vh-100 bg-body-secondary'>
       <div className='col-md-4 mx-auto py-5'>
           <div className="card">
             <div className="card-body">
               <h2 className='text-center my-5'>SIGNUP FORM</h2>
-              <form>
+              <form onSubmit={signupForm.handleSubmit} >
                 <label>Full Name</label>
-                <input type="text" className="form-control mb-3" />
+                <span className='error-label'>{ signupForm.touched.name && signupForm.errors.name}</span>
+                <input type="text" className="form-control mb-3" id='name' onChange={signupForm.handleChange} value={signupForm.values.name} />
                 <label>Email</label>
-                <input type="text" className="form-control mb-3" />
+                <input type="text" className="form-control mb-3" id="email" onChange={signupForm.handleChange} value={signupForm.values.email} />
                 <label>Password</label>
-                <input type="password" className="form-control mb-3" />
+                <span className='error-label'>{ signupForm.touched.password && signupForm.errors.password}</span>
+                <input type="text" className="form-control mb-3" id='password' onChange={signupForm.handleChange} value={signupForm.values.password} />
 
-                <button className='btn btn-primary mt-3'>Submit</button>
+                <button type='submit' className='btn btn-primary mt-3'>Submit</button>
               </form>
             </div>
           </div>
