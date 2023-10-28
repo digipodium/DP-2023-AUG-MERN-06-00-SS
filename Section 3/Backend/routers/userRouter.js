0d50847341eb53323addf0219a2fbd05d46fcab1
m-triple-m <triplem656@gmail.com>
2023-10-28 19:47:02 +0530

    getbyid

--- Section 3/Backend/routers/userRouter.js
+++ Section 3/Backend/routers/userRouter.js
@@ -40,11 +40,28 @@ router.get("/getbyemail/:email", (req, res) => {
       console.log(err);
       res.status(500).json(err);
     });
-
 });
 
-router.get("/getbyid", (req, res) => {
-  res.send("getbyid response from user");
+router.post('/authenticate', (req, res) => {
+  Model.findOne(req.body)
+  .then((result) => {
+    res.status(200).json(result);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
+});
+
+
+router.get("/getbyid/:id", (req, res) => {
+
+  Model.findById(req.params.id)
+  .then((result) => {
+    res.status(200).json(result);
+  }).catch((err) => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.get("/update", (req, res) => {
